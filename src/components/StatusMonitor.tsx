908288e0d6ccc5cf45a7bea4a2153d9e0ea8cbb9
lipitt <victor.david.lip@gmail.com>
2022-04-07 18:32:28 -0300

    removing time field

--- src/components/StatusMonitor.tsx
+++ src/components/StatusMonitor.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const StatusMonitor = ({ name, apiData }) => {
-  const timeStamp = new Date(apiData.time);
-  const time = timeStamp.toLocaleTimeString("es-AR", { hour12: false });
+  // const timeStamp = new Date(apiData.time);
+  // const time = timeStamp.toLocaleTimeString("es-AR", { hour12: false });
 
   return (
     <div>
       <h2>{name}</h2>
       <h2>{apiData.success ? "Healty" : "Error"}</h2>
       <p>{apiData.hostname}</p>
-      <p>{time}</p>
+      {/* <p>{time}</p> */}
     </div>
   );
 };
