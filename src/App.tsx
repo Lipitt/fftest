1d190a0f96ad36fecf29e58f8325d21784180158
lipitt <victor.david.lip@gmail.com>
2022-04-07 18:29:34 -0300

    added all monitors

--- src/App.tsx
+++ src/App.tsx
@@ -1,176 +1,135 @@
 import axios from "axios";
-import React from "react";
-// import StatusMonitor from "./components/StatusMonitor";
+import React, { useEffect, useState } from "react";
+import StatusMonitor from "./components/StatusMonitor";
 
 import "./App.css";
 
 function App() {
-  // const [requestTimeout, setRequestTimeout] = useState(true);
-  // const [assetsData, setAssetsData] = useState("");
-  // const [accountsData, setAccountsData] = useState("");
-  // const [customersData, setCustomersData] = useState("");
-  // const [dataPoints, setDataPoints] = useState("");
-  // const [devices, setDevices] = useState();
-  // const [documents, setDocuments] = useState();
-  // const [forms, setForms] = useState();
-  // const [invites, setInvites] = useState();
-  // const [media, setMedia] = useState();
-  // const [messages, setMessages] = useState();
-  // const [namespaces, setNamespaces] = useState();
-  // const [orders, setOrders] = useState();
-  // const [patients, setPatients] = useState();
-  // const [relationships, setRelationships] = useState();
-  // const [rules, setRules] = useState();
-  // const [teamplates, setTeamplates] = useState();
-  // const [users, setUsers] = useState();
-  // const [workflows, setWorkflows] = useState();
+  const [requestTimeout, setRequestTimeout] = useState(true);
+  const [assetsData, setAssetsData] = useState("");
+  const [accountsData, setAccountsData] = useState("");
+  const [customersData, setCustomersData] = useState("");
+  const [datapointsData, setDatapointsData] = useState("");
+  const [devicesData, setDevicesData] = useState();
+  const [documentsData, setDocumentsData] = useState();
+  const [formsData, setFormsData] = useState();
+  const [invitesData, setInvitesData] = useState();
+  const [mediaData, setMediaData] = useState();
+  const [messagesData, setMessagesData] = useState();
+  const [namespacesData, setNamespacesData] = useState();
+  const [ordersData, setOrdersData] = useState();
+  const [patientsData, setPatientsData] = useState();
+  const [relationshipsData, setRelationshipsData] = useState();
+  const [rulesData, setRulesData] = useState();
+  const [templatesData, setTemplatesData] = useState();
+  const [usersData, setUsersData] = useState();
+  const [workflowsData, setWorkflowsData] = useState();
 
-  // const timerInterval = 15000;
+  const timerInterval = 15000;
 
-  // useEffect(() => {
-  //   if (requestTimeout) {
-  //     fetchDataFromAPI();
-  //     timer();
-  //   }
-  //   setRequestTimeout(false);
-  // }, [requestTimeout]);
+  useEffect(() => {
+    if (requestTimeout) {
+      fetchDataFromAPI();
+      timer();
+    }
+    setRequestTimeout(false);
+  }, [requestTimeout]);
 
-  // const fetchDataFromAPI = () => {
-  //   const setEndpoint = (endpoint: string) => {
-  //     return `https://api.factoryfour.com/${endpoint}/health/status`;
-  //   };
-
-  //   // const accountsEndpoint = `https://api.factoryfour.com/accounts/health/status`;
-  //   // const customersEndpoint = `https://api.factoryfour.com/customers/health/status`;
-  //   // const assetsEndpont = `https://api.factoryfour.com/customers/health/status`
-
-  //   console.log(setEndpoint("accounts"));
-  // const accountsEndpoint = setEndpoint("accounts");
-  // const assetsEndpoint = setEndpoint("assets");
-  // const customersEndpoint = setEndpoint("customers");
-  // const datapointsEndpoint = setEndpoint("datapoints");
-  // const devicesEndpoint = setEndpoint("devices");
-  // const documentsEndpoint = setEndpoint("documents");
-  // const formsEndpoint = setEndpoint("forms");
-  // const invitesEndpoint = setEndpoint("invites");
-  // const mediaEndpoint = setEndpoint("media");
-  // const messagesEndpoint = setEndpoint("messages");
-  // const namespacesEndpoint = setEndpoint("namespaces");
-  // const ordersEndpoint = setEndpoint("orders");
-  // const patientsEndpoint = setEndpoint("patients");
-  // const relationshipsEndpoint = setEndpoint("relationships");
-  // const rulesEndpoint = setEndpoint("accounts");
-  // const templatesEndpoint = setEndpoint("accounts");
-  // const usersEndpoint = setEndpoint("accounts");
-  // const workflowsEndpoint = setEndpoint("accounts");
-
-  // const accountsRes = axios.get(setEndpoint("accounts"));
-  // const assetsRes = axios.get(assetsEndpoint);
-  // const customersRes = axios.get(customersEndpoint);
-  // const datapointsRes = axios.get(datapointsEndpoint);
-  // const devicesRes = axios.get(devicesEndpoint);
-  // const documentsRes = axios.get(documentsEndpoint);
-  // const formsRes = axios.get(formsEndpoint);
-  //const invitesRes = axios.get(invitesEndpoint);
-  //const mediaRes = axios.get(mediaEndpoint);
-  // const messagesRes = axios.get(messagesEndpoint);
-  // const namespacesRes = axios.get(namespacesEndpoint);
-  // const ordersRes = axios.get(ordersEndpoint);
-  // const patientsRes = axios.get(patientsEndpoint);
-  // const relationshipsRes = axios.get(relationshipsEndpoint);
-  // const rulesRes = axios.get(rulesEndpoint);
-  // const templatesRes = axios.get(templatesEndpoint);
-  // const usersRes = axios.get(usersEndpoint);
-  // const workflowsRes = axios.get(workflowsEndpoint);
-
-  //   axios.all([accountsRes, customersRes]).then(
-  //     axios.spread((...allData) => {
-  //       setAccountsData(allData[0].data);
-  //       setCustomersData(allData[1].data);
-  //     })
-  //   );
-  // };
-
-  // const timer = () => {
-  //   setTimeout(() => {
-  //     setRequestTimeout(true);
-  //   }, timerInterval);
-  // };
-
-  const fetchtest = () => {
+  const fetchDataFromAPI = () => {
     const setEndpoint = (endpoint: string) => {
       return `https://api.factoryfour.com/${endpoint}/health/status`;
     };
 
-    // const accountsEndpoint = `https://api.factoryfour.com/accounts/health/status`;
-    // const customersEndpoint = `https://api.factoryfour.com/customers/health/status`;
-    // const assetsEndpont = `https://api.factoryfour.com/customers/health/status`
+    const accountsRes = axios.get(setEndpoint("accounts"));
+    const assetsRes = axios.get(setEndpoint("assets"));
+    const customersRes = axios.get(setEndpoint("customers"));
+    const datapointsRes = axios.get(setEndpoint("datapoints"));
+    const devicesRes = axios.get(setEndpoint("devices"));
+    const documentsRes = axios.get(setEndpoint("documents"));
+    const formsRes = axios.get(setEndpoint("forms"));
+    const invitesRes = axios.get(setEndpoint("invites"));
+    const mediaRes = axios.get(setEndpoint("media"));
+    const messagesRes = axios.get(setEndpoint("messages"));
+    const namespacesRes = axios.get(setEndpoint("namespaces"));
+    const ordersRes = axios.get(setEndpoint("orders"));
+    const patientsRes = axios.get(setEndpoint("patients"));
+    const relationshipsRes = axios.get(setEndpoint("relationships"));
+    const rulesRes = axios.get(setEndpoint("rules"));
+    const templatesRes = axios.get(setEndpoint("templates"));
+    const usersRes = axios.get(setEndpoint("users"));
+    const workflowsRes = axios.get(setEndpoint("workflows"));
 
-    console.log("testapi");
-    const accountsEndpoint = setEndpoint("accounts");
-    const assetsEndpoint = setEndpoint("assets");
-    const customersEndpoint = setEndpoint("customers");
-    const datapointsEndpoint = setEndpoint("datapoints");
-    const devicesEndpoint = setEndpoint("devices");
-    const documentsEndpoint = setEndpoint("documents");
-    const formsEndpoint = setEndpoint("forms");
-    const invitesEndpoint = setEndpoint("invites");
-    const mediaEndpoint = setEndpoint("media");
-    const messagesEndpoint = setEndpoint("messages");
-    const namespacesEndpoint = setEndpoint("namespaces");
-    const ordersEndpoint = setEndpoint("orders");
-    const patientsEndpoint = setEndpoint("patients");
-    const relationshipsEndpoint = setEndpoint("relationships");
-    const rulesEndpoint = setEndpoint("accounts");
-    const templatesEndpoint = setEndpoint("accounts");
-    const usersEndpoint = setEndpoint("accounts");
-    const workflowsEndpoint = setEndpoint("accounts");
+    axios
+      .all([
+        accountsRes,
+        assetsRes,
+        customersRes,
+        datapointsRes,
+        devicesRes,
+        documentsRes,
+        formsRes,
+        invitesRes,
+        mediaRes,
+        messagesRes,
+        namespacesRes,
+        ordersRes,
+        patientsRes,
+        relationshipsRes,
+        rulesRes,
+        templatesRes,
+        usersRes,
+        workflowsRes,
+      ])
+      .then(
+        axios.spread((...allData) => {
+          setAccountsData(allData[0].data);
+          setAssetsData(allData[1].data);
+          setCustomersData(allData[2].data);
+          setDatapointsData(allData[3].data);
+          setDevicesData(allData[4].data);
+          setDocumentsData(allData[5].data);
+          setFormsData(allData[6].data);
+          setInvitesData(allData[7].data);
+          setMediaData(allData[8].data);
+          setMessagesData(allData[9].data);
+          setNamespacesData(allData[10].data);
+          setOrdersData(allData[11].data);
+          setPatientsData(allData[12].data);
+          setRelationshipsData(allData[13].data);
+          setRulesData(allData[14].data);
+          setTemplatesData(allData[15].data);
+          setUsersData(allData[16].data);
+          setWorkflowsData(allData[17].data);
+        })
+      );
+  };
 
-    const accountsRes = axios.get(accountsEndpoint);
-    const assetsRes = axios.get(assetsEndpoint);
-    const customersRes = axios.get(customersEndpoint);
-    const datapointsRes = axios.get(datapointsEndpoint);
-    const devicesRes = axios.get(devicesEndpoint);
-    const documentsRes = axios.get(documentsEndpoint);
-    const formsRes = axios.get(formsEndpoint);
-    const invitesRes = axios.get(invitesEndpoint);
-    const mediaRes = axios.get(mediaEndpoint);
-
-    const messagesRes = axios.get(messagesEndpoint);
-    const namespacesRes = axios.get(namespacesEndpoint);
-    const ordersRes = axios.get(ordersEndpoint);
-    const patientsRes = axios.get(patientsEndpoint);
-    const relationshipsRes = axios.get(relationshipsEndpoint);
-    const rulesRes = axios.get(rulesEndpoint);
-    const templatesRes = axios.get(templatesEndpoint);
-    const usersRes = axios.get(usersEndpoint);
-    const workflowsRes = axios.get(workflowsEndpoint);
-
-    console.log(messagesRes);
-    console.log(namespacesRes);
-    console.log(relationshipsRes);
-    console.log(templatesRes);
-    console.log(workflowsRes);
-    console.log(accountsRes);
-    console.log(assetsRes);
-    console.log(customersRes);
-    console.log(datapointsRes);
-    console.log(devicesRes);
-    console.log(documentsRes);
-    console.log(ordersRes);
-    console.log(patientsRes);
-    console.log(formsRes);
-    console.log(invitesRes);
-    console.log(mediaRes);
-    console.log(usersRes);
-    console.log(rulesRes);
+  const timer = () => {
+    setTimeout(() => {
+      setRequestTimeout(true);
+    }, timerInterval);
   };
 
   return (
     <div className="App">
-      <button onClick={fetchtest}>test api</button>
-      {/* <StatusMonitor name="Accounts" apiData={accountsData} />
-      <StatusMonitor name="Customers" apiData={customersData} /> */}
+      <StatusMonitor name="ACCOUNTS" apiData={accountsData} />
+      <StatusMonitor name="ASSETS" apiData={assetsData} />
+      <StatusMonitor name="CUSTOMERS" apiData={customersData} />
+      <StatusMonitor name="DATAPOINTS" apiData={datapointsData} />
+      <StatusMonitor name="DEVICES" apiData={devicesData} />
+      <StatusMonitor name="DOCUMENTS" apiData={documentsData} />
+      <StatusMonitor name="FORMS" apiData={formsData} />
+      <StatusMonitor name="INVITES" apiData={invitesData} />
+      <StatusMonitor name="MEDIA" apiData={mediaData} />
+      <StatusMonitor name="MESSAGES" apiData={messagesData} />
+      <StatusMonitor name="NAMESPACES" apiData={namespacesData} />
+      <StatusMonitor name="ORDERS" apiData={ordersData} />
+      <StatusMonitor name="PATIENTS" apiData={patientsData} />
+      <StatusMonitor name="RELATIONSHIPS" apiData={relationshipsData} />
+      <StatusMonitor name="RULES" apiData={rulesData} />
+      <StatusMonitor name="TEMPLATES" apiData={templatesData} />
+      <StatusMonitor name="USERS" apiData={usersData} />
+      <StatusMonitor name="WORKFLOWS" apiData={workflowsData} />
     </div>
   );
 }
